ad2a4cb315ceed5bf068f0a054ba4dd57bf09993
Carlos De la Fuente <cdelafuenteh@gmail.com>
2020-08-01 17:32:40 +0200

    little changes in models and login

--- models/recipe.js
+++ models/recipe.js
@@ -1,30 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const recipeSchema = new Schema({
     userId: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "User",
     },
-    nameRecipe: String,
-    typeOfCuisine: String,
-    dishType: String,
-    enum: ["breakfast", "main dish",
-        "side dish", "drink", "dessert", "appetizer"
-    ],
-    difftultyLevel: String,
-    numberofportions: Number,
-    preparationTime: String,
-    cookingTime: Number,
-    ingredients: [String],
-    preparation: String,
-    linkToTheOriginalRecipe: String,
-    notes: String,
-    recipeImage: String,
+    nameRecipe: {
+        type: String
+    },
+    typeOfCuisine: {
+        type: String
+    },
+    dishType: {
+        type: String,
+        enum: [
+            "breakfast",
+            "main course",
+            "side dish",
+            "drink",
+            "dessert",
+            "appetizer",
+            "other",
+        ],
+    },
+    difficultyLevel: {
+        type: String
+    },
+    numberofportions: {
+        type: Number,
+        min: 0
+    },
+    preparationTime: {
+        type: Number,
+        min: 0
+    },
+    cookingTime: {
+        type: Number,
+        min: 0
+    },
+    ingredients: {
+        type: String
+    },
+    preparation: {
+        type: String
+    },
+    linkToTheOriginalRecipe: {
+        type: String
+    },
+    notes: {
+        type: String
+    },
+    recipeImage: {
+        type: String,
+        default: "/models/images/jap8bits.jpg"
+    },
 });
-
-
-
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
